c872aef35c38ded2a1399642870f8a92bdc3e397
KJnong <57115252+KJnong@users.noreply.github.com>
2020-06-05 21:49:52 +0200

    transformes objects form props to array to map

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -2,12 +2,19 @@ import React from 'react'
 import Classes from "./Burger.module.css"
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient"
  
-const Burger= (props)=> {
+const Burger = (props)=> {
+
+    const stateIngredients = Object.keys(props.ingredients)
+    .map(igKey =>{return [...Array(props.ingredients[igKey])]
+        .map((_, i)=>{
+            return <BurgerIngredient key={igKey + i} type={igKey}/>
+        });
+    });
+
     return (
         <div className={Classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="cheese"/>
+            {stateIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     )
